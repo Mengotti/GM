d0b57279f3077d8d341e3f73b903f6a338f1ee8e
Your Name <you@example.com>
2023-05-19 20:13:40 +0200

    Revert estacionGM

---
+++ estacionGM.sql
@@ -0,0 +1,20 @@
+USE [BiciCoruña]
+GO
+
+/****** Object:  Table [dbo].[Estación]    Script Date: 19/05/2023 19:45:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Estación](
+	[Nombre] [varchar](255) NOT NULL,
+	[NumAparcamientos] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Nombre] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
